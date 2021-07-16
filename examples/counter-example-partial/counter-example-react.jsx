2b9db64b9a4ef41890101967eb840c4ce962b19b
Andrew Boyd <andrew@wearebraid.com>
2021-07-16 11:53:51 -0400

    add partial example
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

---
+++ examples/counter-example-partial/counter-example-react.jsx
@@ -0,0 +1,30 @@
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'A React Counting Component',
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState(state => ({
+      count: state.count + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I am an example that only shows a relevant subsection of code.</p>
+        %inject%
+        <p>There is more to my template but it's not revealed in the editor.</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Counter />,
+  document.getElementById('app')
+)
---
+++ examples/counter-example-partial/counter-example-vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="counter">
+    <p>I am an example that only shows a relevant subsection of code.</p>
+    %inject%
+    <p>There is more to my template but it's not revealed in the editor.</p>
+  </div>
+</template>
+
+<script>
+ export default {
+  data () {
+    return {
+      msg: 'I am not specific about being options or composition API',
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
