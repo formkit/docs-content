95dfd64b35fd10a322f07ea573ee0bbf6e52f97b
Andrew Boyd <andrew@wearebraid.com>
2021-11-11 15:24:06 -0500

    adds whitepsace to schema-demo file
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- examples/schema-demo/schema-demo.vue
+++ examples/schema-demo/schema-demo.vue
@@ -9,36 +9,36 @@
 export default {
   data () {
     return {
-// %partial%::js::
-data: {
-  value: 100000,
-  format: (value) => "$" + value.toLocaleString()
-},
-schema: [ // Capsule Schema Format
-  {
-    $el: 'label',
-    children: 'What is your salary?'
-  },
-  {
-    if: '$value >= 90000',
-    then: [{ $el: 'h1', children: ['$format($value)'], attrs: { 'data-range': 'high' } }],
-    else: {
-      if: '$value >= 10000',
-      then: [{ $el: 'h2', children: ['$format($value)'], attrs: { 'data-range': 'medium' } }],
-      else: [{ $el: 'h3', children: 'You need a new job!', attrs: { 'data-range': 'low' } }],
-    },
-  },
-  {
-    $el: 'footer',
-    children: [
-      {
-        $el: 'small',
-        children: '© All rights reserved.'
-      }
-    ]
-  }
-]
-// %partial%::js::
+      // %partial%::js::
+      data: {
+        value: 100000,
+        format: (value) => "$" + value.toLocaleString()
+      },
+      schema: [ // Capsule Schema Format
+        {
+          $el: 'label',
+          children: 'What is your salary?'
+        },
+        {
+          if: '$value >= 90000',
+          then: [{ $el: 'h1', children: ['$format($value)'], attrs: { 'data-range': 'high' } }],
+          else: {
+            if: '$value >= 10000',
+            then: [{ $el: 'h2', children: ['$format($value)'], attrs: { 'data-range': 'medium' } }],
+            else: [{ $el: 'h3', children: 'You need a new job!', attrs: { 'data-range': 'low' } }],
+          },
+        },
+        {
+          $el: 'footer',
+          children: [
+            {
+              $el: 'small',
+              children: '© All rights reserved.'
+            }
+          ]
+        }
+      ]
+      // %partial%::js::
     }
   }
 }
