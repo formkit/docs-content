645af6a1cc1366f8a86444bc4e69a076c932088e
Justin Schroeder <justin@wearebraid.com>
2021-11-14 23:16:01 -0500

    Completes address example

--- examples/group/group.vue
+++ examples/group/group.vue
@@ -7,9 +7,33 @@
       type="text"
       label="Street address"
       name="street"
-      help="What is your street address"
+      placeholder="Your street"
       validation="required"
     />
+    <FormKit
+      type="text"
+      label="City"
+      placeholder="Your city"
+      name="city"
+      validation="required"
+    />
+    <div class="double">
+      <FormKit
+        type="select"
+        label="State"
+        name="state"
+        placeholder="Your state"
+        :options="states"
+        validation="required"
+      />
+      <FormKit
+        type="text"
+        name="zip"
+        label="Zip"
+        placeholder="Your zip"
+        validation="required|number"
+      />
+    </div>
   </FormKit>
   <pre wrap>{{ address }}</pre>
 </template>
@@ -17,4 +41,76 @@
 <script setup>
 import { ref } from 'vue'
 const address = ref({})
+
+const states = {
+    'AL': 'Alabama',
+    'AK': 'Alaska',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'AR': 'Arkansas',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DE': 'Delaware',
+    'DC': 'District Of Columbia',
+    'FM': 'Federated States Of Micronesia',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'IA': 'Iowa',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'ME': 'Maine',
+    'MH': 'Marshall Islands',
+    'MD': 'Maryland',
+    'MA': 'Massachusetts',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MS': 'Mississippi',
+    'MO': 'Missouri',
+    'MT': 'Montana',
+    'NE': 'Nebraska',
+    'NV': 'Nevada',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NY': 'New York',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'MP': 'Northern Mariana Islands',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PW': 'Palau',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VT': 'Vermont',
+    'VI': 'Virgin Islands',
+    'VA': 'Virginia',
+    'WA': 'Washington',
+    'WV': 'West Virginia',
+    'WI': 'Wisconsin',
+    'WY': 'Wyoming'
+}
 </script>
+
+<style>
+.double {
+  display: flex;
+  justify-content: space-between;
+}
+.double > .formkit-outer {
+  width: calc(50% - 0.5em);
+}
+</style>
--- inputs/group.md
+++ inputs/group.md
@@ -1,6 +1,6 @@
 # Group input
 
-The `group` input allows you to structure data from child inputs as an object. The group itself outputs no markup (by default) and can be used in conjunction with any other type of input — including nested [groups](/inputs/groups) and lists.
+The `group` input allows you to structure data from child inputs as an object. The group itself outputs no markup (by default) and can be used in conjunction with any other type of input — including nested [groups](/inputs/groups) and lists. The object keys are the names of the inputs, and the object’s values are each input’s value.
 
 <example
 name="Group input"
@@ -9,14 +9,14 @@ langs="vue"></example>
 
 ## Props & Attributes
 
-The `group` input has no unique props but can make use of the following universal FormKit props.
+The `group` input has no unique props but can make use of some of the following universal FormKit props.
 
-<reference-table input="group">
+<reference-table input="group" :without="['help', 'label', 'validation', 'validation-behavior', 'validation-label']">
 </reference-table>
 
 ## Composition keys
 
-<reference-table type="compositionKeys" primary="composition-key">
+<reference-table type="compositionKeys" primary="composition-key" :without="['outer','label','inner','input','help','messages','message']">
 </reference-table>
 
 ## Available utilities
