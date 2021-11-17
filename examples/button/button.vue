790c3e411f06462cc435e4522f001b6373698e99
Justin Schroeder <justin@wearebraid.com>
2021-11-17 13:42:13 -0500

    Adds button documentation

---
+++ examples/button/button.vue
@@ -0,0 +1,35 @@
+<template>
+<!-- You can use the default slot -->
+<FormKit
+  type="button"
+  help="You can use the default slot"
+>
+  Checkout my slot
+</FormKit>
+
+<!-- You can use the default slot -->
+<FormKit
+  type="button"
+  label="Checkout my label"
+  help="You can use the label prop"
+/>
+
+<!-- You can use the default slot -->
+<FormKit
+  type="button"
+  help="You can bind event listeners"
+  @click="randomColor"
+>
+  Click me!
+</FormKit>
+</template>
+
+<script setup>
+const randomColor = (e) => {
+  const hex = Math.floor(Math.random()*16777215).toString(16)
+  e.target.setAttribute(
+    'style',
+    'background-color: #' + hex
+  )
+}
+</script>
