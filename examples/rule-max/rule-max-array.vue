40974a65c98f7804c1d7279009c37149709daabd
Sasha Milenkovic <sasha@wearebraid.com>
2021-11-18 11:21:25 -0500

    indents option props in rule-max-array

--- examples/rule-max/rule-max-array.vue
+++ examples/rule-max/rule-max-array.vue
@@ -3,11 +3,11 @@
 <FormKit
   type="checkbox"
   :options="[
-	'Pepperoni',
-	'Sausage',
-	'Olives',
-	'Feta',
-	'Mushrooms'
+    'Pepperoni',
+    'Sausage',
+    'Olives',
+    'Feta',
+    'Mushrooms'
   ]"
   label="Select up to 3 pizza toppings"
   validation="max:3"
