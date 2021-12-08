7a6a8a6e29d5edcd73dae6bc627da473da5c9713
Andrew Boyd <andrew@wearebraid.com>
2021-12-08 13:14:52 -0500

    adds hompage one-package.vue example
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

---
+++ examples/homepage/one-package.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- %partial%::html:: -->
+  <FormKit type="form">
+    <FormKit
+      type="text"
+      label="You Name"
+      validation="required"
+      validation-behavior="live"
+    />
+    <FormKit
+      type="select"
+      label="Favorite Food"
+      value="pizza"
+      :options="{
+        pizza: 'Pizza',
+        iceCream: 'Ice Cream',
+        burger: 'Burger'
+      }"
+    />
+    <FormKit
+      type="textarea"
+      label="Special Instructions"
+      help="Got something to say?"
+    />
+  </FormKit>
+  <!-- %partial% -->
+</template>
