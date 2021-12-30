c9405bd9eb1bd8d295e84ccd94acdcdff78e4f5d
Luan Nguyen <luan@wearebraid.com>
2021-12-29 21:45:44 -0500

    Edits to inputs/schema to make the docs more plain.

--- examples/schema-wrappers/schema-wrappers.vue
+++ examples/schema-wrappers/schema-wrappers.vue
@@ -1,15 +1,19 @@
 <template>
-  <FormKit
-    type="checkbox"
-    :schema="{
-      outer: { $el: null },
-      inner: { $el: null },
-      messages: { $el: null },
-      message: { $el: 'div' },
-    }"
-    validation="+accepted"
-    validation-behavior="live"
-    label="Terms and conditions"
-    help="Do you accept our terms and conditions?"
-  />
+<!-- %partial%::html:: -->
+<FormKit
+  type="checkbox"
+  :schema="{
+    outer: { $el: null },
+    inner: { $el: null },
+    messages: { $el: null },
+    message: { $el: 'div' },
+  }"
+  validation="+accepted"
+  validation-behavior="live"
+  label="Terms and conditions"
+  help="Do you accept our terms and conditions?"
+/>
+<!-- Remove the "inner" property of the schema object
+      and see the inner wrapper reappear. -->
+<!-- %partial%::html:: -->
 </template>
