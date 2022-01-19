46adb6d4a5984b36fd70178e11cca4b8a2d13192
Andrew Boyd <andrew@wearebraid.com>
2022-01-19 13:38:22 -0500

    updates localization example to use config file
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

---
+++ examples/locale-change-config/formkit.config.js
@@ -0,0 +1,15 @@
+{
+  locales: {
+    de: {
+      ui: {
+          submit: 'Senden'
+      },
+      validation: {
+          required({ name }) {
+            return `${name[0].toUpperCase() + name.substr(1)} ist ein Pflichtfeld.`
+          },
+          email: 'Dies ist keine gültige E-Mail.'
+      }
+    }
+  }
+}
