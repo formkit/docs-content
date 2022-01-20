596eed96379ddcd8450fc29531e2816fd1343bb8
Andrew Boyd <andrew@wearebraid.com>
2022-01-20 13:56:05 -0500

    rename default playground file
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

---
+++ Playground.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- This editor is using Vue3.
+  Multiple root nodes are supported -->
+  <FormKit
+    type="text"
+    label="FormKit Input"
+    help="edit me to get started"
+  />
+</template>
+
+<script>
+// You can use the options or composition APIs for your component,
+// Including `<script setup>` syntax. ESM Imports are unsupported
+// from URLS as well as secondary playground files.
+export default {}
+</script>
+
+<style>
+/*
+vanilla CSS can go here. If you are using
+Tailwind you can also use @apply rules.
+*/
+</style>
