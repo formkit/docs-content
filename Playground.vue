65689b763e649f3a627e0b262cd206c8f015ee8b
Andrew Boyd <andrew@wearebraid.com>
2022-02-24 10:50:32 -0500

    update playground default file to include scoped styles
    Signed-off-by: Andrew Boyd <andrew@wearebraid.com>

--- Playground.vue
+++ Playground.vue
@@ -1,6 +1,6 @@
 <script setup>
-// ESM Imports are supported from URLS
-// as well as secondary playground files.
+// ESM imports are supported from URLs (https://cdn...)
+// as well as secondary playground files (./OtherFile.vue).
 </script>
 
 <template>
@@ -11,9 +11,11 @@
   />
 </template>
 
-<style>
+<style scoped>
 /*
 vanilla CSS can go here. If you are using
 Tailwind you may also use @apply rules.
+Keep styles scoped to avoid multiple files
+overwriting each other in the render output.
 */
 </style>
