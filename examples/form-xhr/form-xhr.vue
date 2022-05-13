6f142af9a59ac066d8dd8ef86961b4c25ec282c8
Andrew Boyd <andrew@wearebraid.com>
2022-05-13 16:36:24 -0400

    removes duplicate <template> tag from example

--- examples/form-xhr/form-xhr.vue
+++ examples/form-xhr/form-xhr.vue
@@ -30,23 +30,3 @@ async function login (credentials) {
     />
   </FormKit>
 </template>
-
-<template>
-  <FormKit
-    type="form"
-    submit-label="Login"
-    @submit="login"
-  >
-    <FormKit
-      name="email"
-      label="Email address"
-      validation="required|email"
-    />
-    <FormKit
-      type="password"
-      name="password"
-      label="Password"
-      validation="required"
-    />
-  </FormKit>
-</template>
