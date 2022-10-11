789b91452afbb5b8456ea955a4c8b867b2549b8a
Sasha Milenkovic <sasha@wearebraid.com>
2022-10-11 10:47:22 -0400

    Fixes default value for dropdown-full (looks like this api might not be super consistent.

--- examples/dropdown/dropdown-full.vue
+++ examples/dropdown/dropdown-full.vue
@@ -1,33 +1,30 @@
 <script setup>
 async function loadCurrentlyPopularMovies({ page, hasNextPage }) {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f48bcc9ed9cbce41f6c28ea181b67e14&language=en-US&page=${page}`)
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=f48bcc9ed9cbce41f6c28ea181b67e14&language=en-US&page=${page}`
+  )
   if (res.ok) {
     const data = await res.json()
     if (page !== data.total_pages) hasNextPage()
     return data.results.map((item) => ({
-        label: item.title,
-        value: item.id ,
-        poster_path: item.poster_path,
-        overview: item.overview
-      }))
+      label: item.title,
+      value: item.id,
+      poster_path: item.poster_path,
+      overview: item.overview,
+    }))
   }
   return []
 }
 </script>
 
 <template>
-  <FormKit
-    type="form"
-    #default="{ value }"
-    :actions="false"
-  >
+  <FormKit type="form" #default="{ value }" :actions="false">
     <FormKit
       name="currentlyPopularMovie"
       type="dropdown"
       label="Choose a currently popular movie"
       :options="loadCurrentlyPopularMovies"
       :load-on-scroll="true"
-      :value="19995"
     >
       <!--HERE WE ARE DEFINING OUR OPTION SLOT-->
       <template #option="{ option }">
@@ -66,4 +63,3 @@ async function loadCurrentlyPopularMovies({ page, hasNextPage }) {
   font-size: 12px;
 }
 </style>
-
