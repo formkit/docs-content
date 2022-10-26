0f72f2d7b6edb125b0bf4531f8e3b653c351c597
Luan Nguyen <luan@wearebraid.com>
2022-10-26 17:20:37 -0400

    Breaks up button sheet into multiple examples to make it easier to follow

---
+++ examples/button/button-default-slot.vue
@@ -0,0 +1,12 @@
+<template>
+<!-- %partial%::html:: -->
+<!-- The default slot can also set the button label -->
+<FormKit
+  type="button"
+  help="You can use the default slot."
+  prefix-icon="check"
+>
+  Checkout my slot 🔥
+</FormKit>
+<!-- %partial%::html:: -->
+</template>
