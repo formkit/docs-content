0ea2b05244e794e4ecccea2ffc05da1a9bcc5ec4
Luan Nguyen <luan@wearebraid.com>
2022-12-05 16:18:26 -0500

    Updates your-first-form to always recommend the name prop, but not always the id

--- examples/guides/your-first-form/input-name-id/example.vue
+++ examples/guides/your-first-form/input-name-id/example.vue
@@ -1,5 +1,7 @@
 <!-- %partial%::html:: -->
 <template>
+  <!-- Always specify a name  -->
+  <!-- and optionally an id if you need to target a specific input -->
   <FormKit type="text" name="name" id="name" />
 </template>
 <!-- %partial%::html:: -->
