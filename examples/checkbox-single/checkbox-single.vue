82dc67bd1638ecf0508f001ce5173a49920a1d82
Justin Schroeder <justin@formkit.com>
2023-01-12 10:47:14 -0500

    Adds documentation around checking and the checked attribute for single checkboxes. Closes formkit/formkit#237

--- examples/checkbox-single/checkbox-single.vue
+++ examples/checkbox-single/checkbox-single.vue
@@ -1,19 +1,14 @@
-<script setup>
-import { ref } from 'vue'
-const value = ref(true)
-</script>
-
 <template>
-  <!-- %partial%::html:: -->
-  <FormKit
-    v-model="value"
-    type="checkbox"
-    label="Terms and Conditions"
-    help="Do you agree to our terms of service?"
-    name="terms"
-    validation="accepted"
-    validation-visibility="dirty"
-  />
-  <pre wrap>{{ value }}</pre>
-  <!-- %partial%::html:: -->
+  <FormKit type="form" :actions="false" #default="{ value }">
+    <FormKit
+      type="checkbox"
+      label="Terms and Conditions"
+      help="Do you agree to our terms of service?"
+      name="terms"
+      :value="true"
+      validation="accepted"
+      validation-visibility="dirty"
+    />
+    <pre wrap>{{ value }}</pre>
+  </FormKit>
 </template>
