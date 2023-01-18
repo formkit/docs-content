2952afca8c12a5a2cb26bfd28a08b07ebd78f488
Andrew Boyd <andrew@formkit.com>
2023-01-18 13:12:19 -0500

    adds “true” value to multiple attribute on file examples to trigger proper styling

--- examples/file-populate/file-populate.vue
+++ examples/file-populate/file-populate.vue
@@ -4,7 +4,7 @@
     type="file"
     label="Your files"
     help="This input starts with files already “attached”."
-    multiple
+    multiple="true"
     :value="[{ name: 'purple-taste.pdf' }, { name: 'chocolate-recipe.docx' }]"
   />
   <!-- %partial% -->
