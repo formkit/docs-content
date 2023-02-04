8bfcd48549059a538d41e9160447ed305c82ebdd
Andrew Boyd <andrew@formkit.com>
2023-02-04 15:24:18 -0500

    adds documentation for floating labels plugin

---
+++ examples/floating-labels/off-by-default/off-by-default.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Floating labels off by default.</h2>
+  <FormKit type="text" label="Floating Text Label" :floating-label="true" />
+  <FormKit
+    type="textarea"
+    label="Floating Textarea Label"
+    :floating-label="true"
+  />
+  <FormKit type="text" label="I have a standard label" />
+</template>
---
+++ examples/floating-labels/on-by-default/on-by-default.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Floating labels on by default due to plugin option.</h2>
+  <FormKit type="text" label="Floating Text Label" />
+  <FormKit type="textarea" label="Floating Textarea Label" />
+  <FormKit
+    type="text"
+    label="I have a standard label"
+    :floating-label="false"
+  />
+</template>
