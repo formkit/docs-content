f8d9c5e34ec985b0bf3bd51b391e093038f05765
Sasha Milenkovic <sasha@wearebraid.com>
2023-02-28 11:27:58 -0500

    Adds some styling to transfer-list-options-slot example.

--- examples/transfer-list/transfer-list-options-slot.vue
+++ examples/transfer-list/transfer-list-options-slot.vue
@@ -14,6 +14,29 @@ async function getGuests() {
   }
   return []
 }
+
+async function getGuest(id, cachedOption) {
+  console.log('cachedOption', cachedOption)
+  if (cachedOption.hasLoaded) {
+    console.log('return early')
+    return cachedOption
+  }
+  const res = await fetch(`https://api-formkit-docs-examples.formkit.workers.dev/guests/${id}`)
+  if (res.ok) {
+    const data = await res.json()
+    if (data.data) {
+      console.log('data', data.data)
+      return {
+        label: data.data.name,
+        value: data.data.id,
+        age: data.data.age,
+        phone: data.data.phone,
+        email: data.data.email,
+        hasLoaded: true,
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -30,11 +53,37 @@ async function getGuests() {
       source-empty-message="No guests found"
       target-empty-message="No VIPs selected"
       :options="getGuests"
+      :option-loader="getGuest"
     >
       <template #targetOption="{ option }">
-
+        <div class="flex">
+          <div class="item">
+            Name: {{ option.label }}
+          </div>
+          <div class="item">
+            Age: {{ option.age }}
+          </div>
+          <div class="item">
+            Email: {{ option.email }}
+          </div>
+          <div class="item">
+            Phone: {{ option.phone }}
+          </div>
+        </div>
       </template>
     </FormKit>
     <pre>{{ value }}</pre>
   </FormKit>
 </template>
+
+<style scoped>
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.item {
+  font-size: 0.8em;
+  width: 100%;
+}
+</style>
