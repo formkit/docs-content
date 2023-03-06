d158f9ea64dfb69c32a6c874df222798f19a1947
Andrew Boyd <andrew@formkit.com>
2023-03-06 15:28:03 -0500

    adds more homepage example content

---
+++ examples/homepage/architecture/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+import StepOne from './stepOne.vue'
+import StepTwo from './stepTwo.vue'
+import StepThree from './stepThree.vue'
+</script>
+<!-- %partial% -->
+<template>
+  <FormKit type="form" :actions="false">
+    <FormKit type="multi-step" tab-style="progress">
+      <FormKit type="step" name="personalInfo">
+        <StepOne />
+      </FormKit>
+
+      <FormKit type="step" name="references">
+        <StepTwo />
+      </FormKit>
+
+      <FormKit type="step" name="Supplemental">
+        <StepThree />
+        <template #stepNext>
+          <FormKit type="submit" />
+        </template>
+      </FormKit>
+    </FormKit>
+  </FormKit>
+</template>
+<!-- %partial% -->
---
+++ examples/homepage/inputs.vue
@@ -0,0 +1,42 @@
+<script setup>
+const airlines = {
+  american: 'American Airlines',
+  delta: 'Delta',
+  easyjet: 'easyJet',
+  jetblue: 'JetBlue',
+  klm: 'KLM',
+  lufthansa: 'Lufthansa',
+}
+
+const value = {
+  airlines: ['american'],
+}
+</script>
+
+<template>
+  <FormKit type="form" :value="value" :actions="false">
+    <!-- %partial% -->
+    <FormKit
+      label="Airlines"
+      name="airlines"
+      help="Which airline do you prefer to fly on?"
+      type="radio"
+      :options="airlines"
+    >
+      <template #label="context">
+        <img
+          :src="`https://cdn.formk.it/example-assets/airlines/${context.option.value}.png`"
+          width="120"
+          :alt="context.option.label"
+        />
+      </template>
+    </FormKit>
+    <!-- %partial% -->
+  </FormKit>
+</template>
+
+<style scoped>
+img {
+  aspect-ratio: 120 / 46;
+}
+</style>
