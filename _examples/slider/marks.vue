2e9b0bd220c8aa144ae15e052b3fb6005a5d3e09
Andrew Boyd <andrew@formkit.com>
2023-04-21 10:38:53 -0400

    fix typo

--- _examples/slider/marks.vue
+++ _examples/slider/marks.vue
@@ -3,7 +3,7 @@
   <FormKit
     type="slider"
     label="I show marks on my track"
-    help="By default, marks are rendered at every step interal."
+    help="By default, marks are rendered at every step interval."
     value="50"
     step="10"
     :marks="true"
