408ce3f0ca5343b20ee0dc5c171193745b5f3e95
Sasha Milenkovic <sasha@wearebraid.com>
2023-08-16 14:23:30 -0400

    Fixes broken autocomplete selection-slot example.

--- .examples/autocomplete-pro/autocomplete-slot-selection.vue
+++ .examples/autocomplete-pro/autocomplete-slot-selection.vue
@@ -28,7 +28,7 @@ const carBrands = [{ label: 'Audi', value: 'audi', logo: 'https://s3.amazonaws.c
               {{ option.label }}
             </span>
           </div>
-          <button @click.prevent="handlers.removeSelection(option)()" tabindex="-1" type="button" aria-controls="input_1" class="formkit-remove-selection"><span class="formkit-close-icon formkit-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16"><path d="M10,12.5c-.13,0-.26-.05-.35-.15L1.65,4.35c-.2-.2-.2-.51,0-.71,.2-.2,.51-.2,.71,0L10.35,11.65c.2,.2,.2,.51,0,.71-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path><path d="M2,12.5c-.13,0-.26-.05-.35-.15-.2-.2-.2-.51,0-.71L9.65,3.65c.2-.2,.51-.2,.71,0,.2,.2,.2,.51,0,.71L2.35,12.35c-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path></svg></span></button>
+          <button @click.prevent="handlers.removeSelection()()" tabindex="-1" type="button" aria-controls="input_1" class="formkit-remove-selection"><span class="formkit-close-icon formkit-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16"><path d="M10,12.5c-.13,0-.26-.05-.35-.15L1.65,4.35c-.2-.2-.2-.51,0-.71,.2-.2,.51-.2,.71,0L10.35,11.65c.2,.2,.2,.51,0,.71-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path><path d="M2,12.5c-.13,0-.26-.05-.35-.15-.2-.2-.2-.51,0-.71L9.65,3.65c.2-.2,.51-.2,.71,0,.2,.2,.2,.51,0,.71L2.35,12.35c-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path></svg></span></button>
         </div>
       </template>
     <!--/SELECTION SLOT-->
