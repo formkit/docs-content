712fb983d52d70f16dc3f6fd352f125c602dda4b
Justin Schroeder <justin@formkit.com>
2023-08-24 20:59:44 -0400

    Adds numeric casting to text input

---
+++ .examples/text/text-cast.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormKit type="form" :actions="false" #default="{ value }">
+    <!-- %partial%::html:: -->
+    <FormKit
+      type="text"
+      label="Atmospheric pressure"
+      number
+      name="pressure"
+      help="My value will be a number if it can be parsed by parseFloat"
+      value="29.82"
+    />
+    <pre wrap>{{ JSON.stringify(value) }}</pre>
+    <!-- %partial%::html:: -->
+  </FormKit>
+</template>
