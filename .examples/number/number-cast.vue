e1907cf9bf6906b887c18da1d6826eff761fde52
Justin Schroeder <justin@formkit.com>
2023-08-24 20:19:42 -0400

    Adds float and integer to number docs

--- .examples/number/number-cast.vue
+++ .examples/number/number-cast.vue
@@ -1,16 +1,23 @@
 <template>
-  <FormKit type="form" :actions="false" #default="{ age }">
+  <FormKit type="form" :actions="false" #default="{ value: values }">
     <!-- %partial%::html:: -->
     <FormKit
       type="number"
-      help="I will always be an integer"
-      label="Age"
-      name="age"
-      :value="25"
-      step="1"
-      number="integer"
+      help="I will always parse as a float"
+      label="Spoons of sugar (float)"
+      number
+      name="sugar_float"
+      value="3.25"
     />
-    <pre wrap>age: {{ age }}</pre>
+    <FormKit
+      type="number"
+      help="I will always be an integer"
+      label="Age (integer)"
+      number="integer"
+      name="age_integer"
+      value="25"
+    />
+    <pre wrap>{{ JSON.stringify(values) }}</pre>
     <!-- %partial%::html:: -->
   </FormKit>
 </template>
