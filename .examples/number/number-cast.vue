98b9bd4bab39637ceb1f369aafe4f3d24ab7503c
Justin Schroeder <justin@formkit.com>
2023-08-24 20:13:22 -0400

    Adds number prop details to number input

---
+++ .examples/number/number-cast.vue
@@ -0,0 +1,16 @@
+<template>
+  <FormKit type="form" :actions="false" #default="{ age }">
+    <!-- %partial%::html:: -->
+    <FormKit
+      type="number"
+      help="I will always be an integer"
+      label="Age"
+      name="age"
+      :value="25"
+      step="1"
+      number="integer"
+    />
+    <pre wrap>age: {{ age }}</pre>
+    <!-- %partial%::html:: -->
+  </FormKit>
+</template>
