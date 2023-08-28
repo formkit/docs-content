0256ab0ce90ff4bb24f194ff4b361bf484b8948d
Justin Schroeder <justin@formkit.com>
2023-08-28 17:11:06 -0400

    Fixes tag example

--- .examples/taglist/taglist-full-example.vue
+++ .examples/taglist/taglist-full-example.vue
@@ -42,16 +42,16 @@ async function searchMovies({ search, page, hasNextPage }) {
     >
       <!--HERE WE ARE DEFINING OUR TAG SLOT-->
       <template #tag="{ handlers, option, classes }">
-        <div :class="formkit.tag">
+        <div :class="classes.tag">
           <img
-            :class="formkit.tagImage"
+            :class="classes.tagImage"
             :src="`https://image.tmdb.org/t/p/w500${option.poster_path}`"
             alt="optionAvatar"
           />
-          <span :class="formkit.tagLabel">
+          <span :class="classes.tagLabel">
             {{ option.label }}
           </span>
-          <button @click.prevent="handlers.removeSelection(option)()" tabindex="-1" type="button" aria-controls="input_1" class="formkit-remove-selection"><span class="formkit-close-icon formkit-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16"><path d="M10,12.5c-.13,0-.26-.05-.35-.15L1.65,4.35c-.2-.2-.2-.51,0-.71,.2-.2,.51-.2,.71,0L10.35,11.65c.2,.2,.2,.51,0,.71-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path><path d="M2,12.5c-.13,0-.26-.05-.35-.15-.2-.2-.2-.51,0-.71L9.65,3.65c.2-.2,.51-.2,.71,0,.2,.2,.2,.51,0,.71L2.35,12.35c-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path></svg></span></button>
+          <button @click.prevent="handlers.removeSelection(option)()" tabindex="-1" type="button" aria-controls="input_1" :class="classes.removeSelection"><span :class="`${classes.closeIcon} ${classes.Icon}`"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16"><path d="M10,12.5c-.13,0-.26-.05-.35-.15L1.65,4.35c-.2-.2-.2-.51,0-.71,.2-.2,.51-.2,.71,0L10.35,11.65c.2,.2,.2,.51,0,.71-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path><path d="M2,12.5c-.13,0-.26-.05-.35-.15-.2-.2-.2-.51,0-.71L9.65,3.65c.2-.2,.51-.2,.71,0,.2,.2,.2,.51,0,.71L2.35,12.35c-.1,.1-.23,.15-.35,.15Z" fill="currentColor"></path></svg></span></button>
         </div>
       </template>
       <!---->
