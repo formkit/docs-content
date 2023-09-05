210bfb3489348f1b725779780fa8a7fe96330967
Andrew Boyd <andrew@formkit.com>
2023-09-05 10:03:45 -0400

    remove style tag CSS imports for barcode examlpe

--- .examples/barcode/formats.vue
+++ .examples/barcode/formats.vue
@@ -14,8 +14,3 @@
   />
   <!-- %partial% -->
 </template>
-
-<style>
-/* this import is only neccessary for the playground */
-@import 'https://cdn.jsdelivr.net/npm/@formkit/barcode@latest/dist/assets/barcodeReader.css';
-</style>
