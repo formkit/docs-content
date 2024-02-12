a25f5f3374a97b9702cb9e800aa4c09483bf2bdf
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-02-12 15:53:42 -0300

    docs/add-example
    Adds programatic navigation example

---
+++ .examples/multi-step/programatic-navigation/programatic-navigation.vue
@@ -0,0 +1,43 @@
+<!-- %partial% -->
+<script setup>
+import { getNode } from '@formkit/core'
+
+const next = () => {
+  const node = getNode('multi-step')
+  node.next()
+}
+
+const previous = () => {
+  const node = getNode('multi-step')
+  node.previous()
+}
+
+const goTo = () => {
+  const node = getNode('multi-step')
+  node.goTo('favoriteThing')
+}
+</script>
+<!-- %partial% -->
+
+<template>
+  <FormKit
+    type="multi-step"
+    id="multi-step"
+  >
+    <FormKit type="step" name="personalInfo" id="personalInfo">
+      <FormKit type="text" label="Name" prefix-icon="avatarMan" />
+    </FormKit>
+
+    <FormKit type="step" name="favoriteThing" id="favoriteThing">
+      <FormKit type="text" label="Favorite Thing" prefix-icon="star" />
+    </FormKit>
+
+    <FormKit type="step" name="happyMemory" id="happyMemory">
+      <FormKit type="text" label="Happy Memory" prefix-icon="happy" />
+    </FormKit>
+  </FormKit>
+
+  <FormKit type="button" @click="next" label="Next" />
+  <FormKit type="button" @click="previous" label="Previous" />
+  <FormKit type="button" @click="goTo" label="Go to Favorite thing" />
+</template>
