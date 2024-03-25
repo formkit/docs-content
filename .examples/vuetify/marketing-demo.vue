9fb630e4698345ed6a698a9a75be6b73e5e98255
Andrew Boyd <andrew@formkit.com>
2024-03-25 14:29:21 -0400

    adds vuetify example files

---
+++ .examples/vuetify/marketing-demo.vue
@@ -0,0 +1,22 @@
+<script setup>
+/** playground setup for vuetify specifically **/
+import { createVuetify } from 'vuetify'
+const vuetify = createVuetify({
+  // components: {}
+})
+window.vueApp.use(vuetify)
+</script>
+
+<template>
+  <!-- %partial% -->
+  <VFormKit>
+    <h2 class="text-lg font-bold text-center mb-3">FormKit Vuetify Demo</h2>
+    <VTextField variant="outlined" />
+  </VFormKit>
+  <!-- %partial% -->
+</template>
+
+<style scoped>
+/** Vuetify CSS **/
+@import 'https://cdn.jsdelivr.net/npm/vuetify@latest/dist/vuetify.min.css';
+</style>
